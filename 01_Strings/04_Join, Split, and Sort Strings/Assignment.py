0ef414bdfd1c9dd0f0fa363961c222a0ec034cf0
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-11 07:59:29 -0500

    updates

--- 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
+++
@@ -1,9 +0,0 @@
-#YOUDO:  create first_name and last_name variable and 
-# assign values using the input function
-first_name = ""
-last_name = ""
-
-sep = " "
-full_name = ""  #YOUDO use the join function to 
-#join together first_name and last_name 
-print(full_name)
