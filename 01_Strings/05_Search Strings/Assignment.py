4886b81e3f78b53e95d0981af952b4fa2cc10b59
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-25 07:53:12 -0500

    updates

---
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -0,0 +1,10 @@
+text = "" #use input to actually store into text.  remove the ""
+
+result = text.find("the")
+
+if result == -1:
+    print("The word the is not in the string")
+else:
+    print("The word the is in the string")
+    print("It is located at", )
+    #YOUDO.  print out where the word 'the' appears in the string.  
---
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -0,0 +1,20 @@
+number1 = 0
+number2 = 0
+try:
+    number1 = int(input("Enter a number"))
+    # YOUDO.  use input function and int to set number2
+except ValueError:
+    # print message stating that an integer wasn't given
+    pass #YOUDO remove pass when done
+
+
+try:
+    #YOUDO divide number1 / number2 and set to answer
+    #YOUDO  print the result of the division (aka answer with some helper text)
+    pass  # YOUDO remove pass when done
+
+except ZeroDivisionError:
+    #YOUDO:  print message stating that division by zero is not possible.  
+    pass  # YOUDO remove pass when done
+
+    
