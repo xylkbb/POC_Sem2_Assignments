6275b7b15ed5ca081906180a81eb84ebb069f104
xylkbb <coeeri001@warren.k12.in.us>
2024-02-05 07:59:25 -0500

    update

---
+++ 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
@@ -0,0 +1,7 @@
+def average(a : float, b: float) -> float:
+    return (a + b) / 2
+
+
+number1 = float(input("Enter a decimal number: "))
+number2 = float(input("Enter another deciaml number: "))
+print(average(number1, number2))
