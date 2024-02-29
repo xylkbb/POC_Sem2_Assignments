0bdc6594affe4e3d9585080478fccc179e8e8b66
xylkbb <coeeri001@warren.k12.in.us>
2024-02-29 08:02:01 -0500

    update

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,15 +1,43 @@
+""" class RightTriangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    
+    def area(self):
+        print(" ", self.base * self.height)
+
+
+
+
+tri1 = RightTriangle(1, 2)
+tri2 = RightTriangle(4, 6)
+
+print("The base of tri1 is ", tri1.base)
+print("The height of tri1 is", tri1.height)
+print("The area of tri1 is " , tri1.area())
+ """
+
+
 class RightTriangle:
     def __init__(self, base, height):
         self.base = base
         self.height = height
     
-    def CalculateArea(self):
-        print("The area is", self.base * self.height, "!!")
+    def area(self):
+        print("The area to tri1 is", self.base * self.height, "!!")
+    def area2(self):
+        print("The area to tri2 is", self.base * self.height, "!!")
+         
 
 
 
-righttriangeone = RightTriangle(1, 2)
-righttriangetwo = RightTriangle(4, 6)
+tri1 = RightTriangle(1, 2)
+tri2 = RightTriangle(4, 6)
 
-righttriangeone.CalculateArea()
-righttriangetwo.CalculateArea()
+print("The base to tri1 is ", tri1.base)
+print("The height to tri1 is ", tri1.height)
+tri1.area()
+print( "\n")
+print("The base to tri2 is ", tri2.base)
+print("The height to tri2 is ", tri2.height)
+tri2.area()
