f88d42f189880be2372bdc52a222a78d20e559c0
xylkbb <coeeri001@warren.k12.in.us>
2024-02-29 08:02:41 -0500

    updatefinally

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,23 +1,3 @@
-""" class RightTriangle:
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
-    
-    def area(self):
-        print(" ", self.base * self.height)
-
-
-
-
-tri1 = RightTriangle(1, 2)
-tri2 = RightTriangle(4, 6)
-
-print("The base of tri1 is ", tri1.base)
-print("The height of tri1 is", tri1.height)
-print("The area of tri1 is " , tri1.area())
- """
-
-
 class RightTriangle:
     def __init__(self, base, height):
         self.base = base
