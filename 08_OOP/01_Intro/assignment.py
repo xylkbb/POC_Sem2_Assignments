0f9f89d1b5d5dc6c60f61d677eb4a3982fb4b131
xylkbb <coeeri001@warren.k12.in.us>
2024-03-04 07:54:49 -0500

    update

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,23 +1,31 @@
+from math import hypot
+
 class RightTriangle:
     def __init__(self, base, height):
         self.base = base
         self.height = height
     
     def area(self):
-        print("The area to tri1 is", self.base * self.height, "!!")
-    def area2(self):
-        print("The area to tri2 is", self.base * self.height, "!!")
-         
+        return self.base * self.height
+    
+    def hypotenuse(self):
+        return hypot(self.base, self.height)
+        
+    def perimeter(self):
+        return self.base + self.height + self.hypotenuse()
 
 
-
-tri1 = RightTriangle(1, 2)
+tri1 = RightTriangle(3, 4)
 tri2 = RightTriangle(4, 6)
 
-print("The base to tri1 is ", tri1.base)
-print("The height to tri1 is ", tri1.height)
-tri1.area()
-print( "\n")
-print("The base to tri2 is ", tri2.base)
-print("The height to tri2 is ", tri2.height)
-tri2.area()
+print("The base of triangle 1 is", tri1.base, "and", tri1.height, "\nThe area is", tri1.area())
+print("The hypotenuse is", tri1.hypotenuse())
+print("The perimeter is ", tri1.perimeter())
+
+print("----------------------")
+
+print("The base of triangle 2 is", tri2.base, "and", tri2.height, "\nThe area is", tri2.area())
+print("The hypotenuse is", tri2.hypotenuse())
+print("The perimeter is ", tri2.perimeter())
+
+
