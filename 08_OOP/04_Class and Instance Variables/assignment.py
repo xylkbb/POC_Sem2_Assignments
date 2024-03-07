64f179e6161114a22d61222fca2532644051163f
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-07 06:55:45 -0500

    updates

---
+++ 08_OOP/04_Class and Instance Variables/assignment.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, base: float, height: float) -> None:
+        if(base < 0):
+            self.__base = 0
+        else:       
+            self.__base = base
+            
+        if(height < 0):
+            self.__height = 0
+        else:
+            self.__height = height
+        
+    def get_height(self) -> float:
+        return self.__height
+    
+    #YOUDO the get_base method
+    
+    def get_perimeter(self) -> float:
+        return 2 * self.__base + 2 * self.__height
+    
+    #Youdo get_area method
+ 
+ 
+ 
+#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
+#using only the methods not the fields/property/attributes
