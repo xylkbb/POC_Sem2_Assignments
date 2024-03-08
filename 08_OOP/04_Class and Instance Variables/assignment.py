0012a5bccc1a7bb196642bdba4cbfa20ee9c33d8
xylkbb <coeeri001@warren.k12.in.us>
2024-03-08 07:30:52 -0500

    update

--- 08_OOP/04_Class and Instance Variables/assignment.py
+++ 08_OOP/04_Class and Instance Variables/assignment.py
@@ -1,26 +1,35 @@
 class Rectangle:
+    __counter = 0
+
+    def get_count():
+        return Rectangle.__counter
+
     def __init__(self, base: float, height: float) -> None:
-        if(base < 0):
-            self.__base = 0
-        else:       
-            self.__base = base
-            
-        if(height < 0):
-            self.__height = 0
-        else:
-            self.__height = height
-        
-    def get_height(self) -> float:
-        return self.__height
+        self.__base = base
+        self.__height = height
+        Rectangle.__counter += 1
+
+    def get_base(self) -> float: 
+        return self.__base
     
-    #YOUDO the get_base method
-    
-    def get_perimeter(self) -> float:
+    def get_height(self) -> float: 
+        return self.__height    
+
+
+    def area(self) -> float:
+        return self.__base * self.__height
+
+    def perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-    
-    #Youdo get_area method
- 
- 
- 
-#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-#using only the methods not the fields/property/attributes
+
+
+rect1 = Rectangle(5, 3)
+
+rect2 = Rectangle(6, 8)
+
+print("The area of rectangle 1 is ", rect1.area())
+print("The perimeter of rectangle 1 is", rect1.perimeter())
+print("_________________")
+print("The area of rectangle 2 is", rect2.area())
+print("The perimeter of rectnalge 2 is", rect2.perimeter())
+print(Rectangle.get_count())
