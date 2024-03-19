c87d4ac8f2cc0c1b13e15709ca803d420a8d9c6a
xylkbb <coeeri001@warren.k12.in.us>
2024-03-19 07:29:15 -0400

    go goated

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -1,5 +1,4 @@
 class Rectangle:
-<<<<<<< HEAD
     __counter = 0
 
     def get_count():
@@ -25,7 +24,7 @@ class Rectangle:
         return 2 * self.__base + 2 * self.__height
 
     def __str__(self) -> str:
-        return "Rectangle of base:" + self.__base + ", height ", +self.__height
+        return "Rectangle of base:" + self.__base + ", height ", + self.__height
 
 
 
@@ -43,35 +42,3 @@ print("The area of rectangle 2 is", rect2.area())
 print("The perimeter of rectnalge 2 is", rect2.perimeter())
 print(Rectangle.get_count())
 print("_________________")
-=======
-
-    def __init__(self, base: float, height: float) -> None:
-        if (base < 0):
-            self.__base = 0
-        else:
-            self.__base = base
-
-        if (height < 0):
-            self.__height = 0
-        else:
-            self.__height = height
-
-    def get_height(self) -> float:
-        return self.__height
-
-    # YOUDO the get_base method
-
-    def get_perimeter(self) -> float:
-        return 2 * self.__base + 2 * self.__height
-
-    # Youdo get_area method
-    
-    def __str__(self) -> str:
-        #Rectangle with base:3, height:4
-        return "Rectangle with base:" + str(self.__base) + ", height:" + str(self.__height)
-    
-
-
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
->>>>>>> 73a7eaf95d67ac9eec084cec55955e01fa981183
---
+++ 10_Functional Programming/01_Lambda Expressions/assignment.py
@@ -0,0 +1,7 @@
+product = lambda x, y: x * y
+quotient = lambda x, y: x / y
+exponent = lambda x, y: x ** y
+
+print(product(15, 13))
+print(quotient(15, 13))
+print(exponent(3, 7))
