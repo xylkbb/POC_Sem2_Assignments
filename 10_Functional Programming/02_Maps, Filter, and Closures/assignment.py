62c5c49131800e7eba24bad8aa9cac64d1c9aff4
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-20 07:52:22 -0400

    updates

---
+++ 10_Functional Programming/02_Maps, Filter, and Closures/assignment.py
@@ -0,0 +1,14 @@
+def times2(a): return a * 2
+def div2(a): return a / 2
+def exp2(a): return a ** 2
+
+my_list = [1, 2] #You make
+
+mapped_times2 = map(times2, my_list)
+#YOUDO:  repeate for div2, exp2
+
+mapped_times2_list = list(mapped_times2)
+print(mapped_times2_list)
+#YOUDO:  repeat for the other maps 
+
+
