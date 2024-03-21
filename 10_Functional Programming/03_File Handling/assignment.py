04ce4fa0a26a853279aef7b74c410b43e25b566d
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-21 07:53:54 -0400

    updates

---
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -0,0 +1,4 @@
+file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\myFile.txt"
+
+##YOUDO rest of the program for reading and printing to the console
+##remember pass in file_path to open 
